d7d0230193ab36f24934ac09fc74a5fa6f75b7aa
gyojincompany <luckyred@gmail.com>
2024-01-22 16:26:38 +0900

    타임리프 문법 연습

---
+++ src/test/java/com/gyojincompany/home/ThymeleafTest20240122ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTest20240122ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
